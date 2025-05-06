11ffc64efd6e0c88d494cd505a4326dc8daf369e
Fernando Becerra <becerrafernando@gmail.com>
2025-05-06 10:40:15 -0300

    add private specific routines

---
+++ oda_reader/private.py
@@ -0,0 +1,51 @@
+import pandas as pd
+
+from oda_reader._cache import cache_info
+from oda_reader.download.download_tools import download
+
+DATAFLOW_ID: str = "DSD_PPFD@DF_PPFD"
+DATAFLOW_VERSION: str = "1.4"
+
+
+@cache_info
+def download_private(
+    start_year: int | None = None,
+    end_year: int | None = None,
+    filters: dict | None = None,
+    pre_process: bool = True,
+    dotstat_codes: bool = True,
+    dataflow_version: str | None = None,
+) -> pd.DataFrame:
+    """
+    Download the Private donors data from the API.
+
+    Args:
+        start_year (int): The start year of the data to download. Optional
+        end_year (int): The end year of the data to download. Optional
+        filters (dict): Optional filters to pass to the download.
+        pre_process (bool): Whether to preprocess the data. Defaults to True.
+        Preprocessing makes it comply with the .stat schema.
+        dotstat_codes (bool): Whether to convert the donor codes to the .stat schema.
+        dataflow_version (str): The version of the dataflow. Optional
+
+    Returns:
+        pd.DataFrame: The Private donors data.
+
+    """
+
+    # Inform of the dataflow being downloaded
+    if dataflow_version is None:
+        dataflow_version = DATAFLOW_VERSION
+
+    df = download(
+        version="private",
+        dataflow_id=DATAFLOW_ID,
+        start_year=start_year,
+        end_year=end_year,
+        filters=filters,
+        pre_process=pre_process,
+        dotstat_codes=dotstat_codes,
+        dataflow_version=dataflow_version,
+    )
+
+    return df
---
+++ oda_reader/schemas/private_translation.py
@@ -0,0 +1,77 @@
+import pandas as pd
+
+from oda_reader.common import ImporterPaths
+from oda_reader.schemas.dac1_translation import prices_mapping
+from oda_reader.schemas.schema_tools import map_area_codes, map_amount_type_codes
+from oda_reader.schemas.xml_tools import (
+    parse_xml,
+    extract_dac_to_area_codes,
+    read_mapping,
+)
+
+DAC2_TRANSLATION_SCHEMA_URL = (
+    "https://stats.oecd.org/FileView2.aspx?IDFile=997ad7fb-48f1-4046-945d-067dc5bec7de"
+)
+
+MAPPINGS = {
+    "private_codes_area": ImporterPaths.mappings / "private_codes_area.json",
+    "area_code_corrections": ImporterPaths.mappings / "area_code_corrections.json",
+}
+
+
+def update_private_translation_mappings():
+    """Pipeline to update the DAC2A translation mappings"""
+    xml_data = parse_xml(xml_url=DAC2_TRANSLATION_SCHEMA_URL)["Structures"]
+
+    # oecd dac donor codes to area codes
+    extract_dac_to_area_codes(xml_dict=xml_data, filename=MAPPINGS["private_codes_area"])
+
+
+def area_code_mapping() -> dict:
+    """Reads the area code mapping."""
+    return read_mapping(
+        MAPPINGS["private_codes_area"],
+        keys_as_int=True,
+        update=update_private_translation_mappings,
+    ) | read_mapping(
+        MAPPINGS["area_code_corrections"],
+        keys_as_int=True,
+        update=update_private_translation_mappings,
+    )
+
+
+def convert_private_to_dotstat_codes(df: pd.DataFrame) -> pd.DataFrame:
+    """Convert the Private donor data to the .stat schema.
+    Args:
+        df (pd.DataFrame): The Private donor data.
+
+    Returns:
+        pd.DataFrame: The Private donor data in the .stat schema.
+    """
+    # # Get the area codes
+    # area_codes = area_code_mapping()
+
+    # # Prices mapping
+    # prices_codes = prices_mapping()
+
+    # # Map the donor codes
+    # df = map_area_codes(df, area_code_mapping=area_codes)
+
+    # # Map region codes
+    # df = map_area_codes(
+    #     df,
+    #     area_code_mapping=area_codes,
+    #     source_column="recipient_code",
+    #     target_column="recipient_code",
+    # )
+
+    # # Map the prices codes
+    # df = map_amount_type_codes(
+    #     df,
+    #     prices_mapping=prices_codes,
+    #     source_column="data_type_code",
+    #     target_column="data_type_code",
+    # )
+
+    # return df
+    return None
