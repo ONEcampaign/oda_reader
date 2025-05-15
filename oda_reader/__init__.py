66f64b76818d9ef9559b8c4f80d40be112c64cf4
Jorge Rivera <jorge.rivera@one.org>
2025-05-15 10:59:08 +0200

    bump version

--- oda_reader/__init__.py
+++ oda_reader/__init__.py
@@ -3,7 +3,7 @@ This oda_reader package is a simple python wrapper for the OECD explorer API,
 specifically designed to work with OECD DAC data.
 """
 
-__version__ = "1.1.4"
+__version__ = "1.1.5"
 
 from oda_reader._cache import (
     enforce_cache_limits,
